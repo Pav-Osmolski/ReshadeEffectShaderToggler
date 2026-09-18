fdebd15decadaa726cf7386ba9140e0afe5e021c
Pawel Osmolski <48226562+Pav-Osmolski@users.noreply.github.com>
2026-09-18 20:43:08 +0100

    Track recent resource barriers for scene-colour discovery

--- src/StateTracking.h
+++ src/StateTracking.h
@@ -113,6 +113,14 @@ struct state_block {
 
     std::unordered_map<uint64_t, barrier_track> resource_barrier_track;
 
+    struct barrier_history_entry {
+        uint64_t serial = 0;
+        reshade::api::resource_usage old_usage = reshade::api::resource_usage::undefined;
+        reshade::api::resource_usage new_usage = reshade::api::resource_usage::undefined;
+    };
+    uint64_t barrier_serial = 0;
+    std::unordered_map<uint64_t, barrier_history_entry> barrier_history;
+
     struct render_target_history_entry {
         uint64_t bind_serial = 0;
         uint32_t slot = 0;
