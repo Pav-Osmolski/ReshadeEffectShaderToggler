a3084fc19ed226620a9a9550d47251829f45b118
Pawel Osmolski <48226562+Pav-Osmolski@users.noreply.github.com>
2026-09-20 14:46:02 +0100

    Add Vulkan deferred preview API

--- src/RenderingPreviewManager.h
+++ src/RenderingPreviewManager.h
@@ -10,6 +10,8 @@ class __declspec(novtable) RenderingPreviewManager final {
     ~RenderingPreviewManager();
 
     void UpdatePreview(reshade::api::command_list* cmd_list, uint64_t callLocation, uint64_t invocation);
+    void RecordVulkanHuntedTarget(reshade::api::command_list* cmd_list, uint32_t stageIndex, uint32_t shaderHash);
+    void CaptureDeferredVulkanPreview(reshade::api::command_list* cmd_list);
     const ResourceViewData GetCurrentPreviewResourceView(reshade::api::command_list* cmd_list,
                                                          DeviceDataContainer& deviceData,
                                                          const ShaderToggler::ToggleGroup* group,
