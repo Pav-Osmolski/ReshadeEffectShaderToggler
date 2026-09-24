7f363c1689b297de2d0165d236b1792acff630dd
Pawel Osmolski <48226562+Pav-Osmolski@users.noreply.github.com>
2026-09-24 17:14:00 +0100

    legacy: use ReShade 5.8 descriptor-set API

--- src/DescriptorTracking.cpp
+++ src/DescriptorTracking.cpp
@@ -9,8 +9,8 @@
 
 using namespace reshade::api;
 
-sampler descriptor_tracking::get_sampler(descriptor_heap heap, uint32_t offset) const {
-    const descriptor_heap_data& heap_data = heaps.at(heap);
+sampler descriptor_tracking::get_sampler(descriptor_pool heap, uint32_t offset) const {
+    const descriptor_pool_data& heap_data = heaps.at(heap);
 
     if (offset < heap_data.descriptors.size()) {
         const descriptor_data& descriptor = heap_data.descriptors[offset];
@@ -23,8 +23,8 @@ sampler descriptor_tracking::get_sampler(descriptor_heap heap, uint32_t offset) 
 
     return { 0 };
 }
-resource_view descriptor_tracking::get_shader_resource_view(descriptor_heap heap, uint32_t offset) const {
-    const descriptor_heap_data& heap_data = heaps.at(heap);
+resource_view descriptor_tracking::get_shader_resource_view(descriptor_pool heap, uint32_t offset) const {
+    const descriptor_pool_data& heap_data = heaps.at(heap);
 
     if (offset < heap_data.descriptors.size()) {
         const descriptor_data& descriptor = heap_data.descriptors[offset];
@@ -37,8 +37,8 @@ resource_view descriptor_tracking::get_shader_resource_view(descriptor_heap heap
 
     return { 0 };
 }
-buffer_range descriptor_tracking::get_buffer_range(descriptor_heap heap, uint32_t offset) const {
-    const descriptor_heap_data& heap_data = heaps.at(heap);
+buffer_range descriptor_tracking::get_buffer_range(descriptor_pool heap, uint32_t offset) const {
+    const descriptor_pool_data& heap_data = heaps.at(heap);
 
     if (offset < heap_data.descriptors.size()) {
         const descriptor_data& descriptor = heap_data.descriptors[offset];
@@ -50,12 +50,12 @@ buffer_range descriptor_tracking::get_buffer_range(descriptor_heap heap, uint32_
     return { 0 };
 }
 
-void descriptor_tracking::set_all_descriptors(reshade::api::descriptor_heap heap,
+void descriptor_tracking::set_all_descriptors(reshade::api::descriptor_pool heap,
                                               uint32_t offset,
                                               uint32_t count,
                                               std::vector<descriptor_tracking::descriptor_data>& descriptor_list,
                                               uint32_t list_offset) const {
-    const descriptor_heap_data& heap_data = heaps.at(heap);
+    const descriptor_pool_data& heap_data = heaps.at(heap);
 
     size_t mcount = std::min(static_cast<size_t>(offset) + count, heap_data.descriptors.size());
 
@@ -76,9 +76,9 @@ void descriptor_tracking::register_pipeline_layout(pipeline_layout layout, uint3
     layout_data.ranges.resize(count);
 
     for (uint32_t i = 0; i < count; ++i) {
-        if (params[i].type == pipeline_layout_param_type::descriptor_table) {
-            layout_data.ranges[i].assign(params[i].descriptor_table.ranges, params[i].descriptor_table.ranges + params[i].descriptor_table.count);
-            layout_data.params[i].descriptor_table.ranges = layout_data.ranges[i].data();
+        if (params[i].type == pipeline_layout_param_type::descriptor_set) {
+            layout_data.ranges[i].assign(params[i].descriptor_set.ranges, params[i].descriptor_set.ranges + params[i].descriptor_set.count);
+            layout_data.params[i].descriptor_set.ranges = layout_data.ranges[i].data();
         }
     }
 }
@@ -104,22 +104,22 @@ void descriptor_tracking::on_destroy_pipeline_layout(device* device, pipeline_la
     ctx.unregister_pipeline_layout(layout);
 }
 
-bool descriptor_tracking::on_copy_descriptor_tables(device* device, uint32_t count, const descriptor_table_copy* copies) {
+bool descriptor_tracking::on_copy_descriptor_sets(device* device, uint32_t count, const descriptor_set_copy* copies) {
     descriptor_tracking& ctx = device->get_private_data<descriptor_tracking>();
 
     for (uint32_t i = 0; i < count; ++i) {
-        const descriptor_table_copy& copy = copies[i];
+        const descriptor_set_copy& copy = copies[i];
 
         uint32_t src_offset;
-        descriptor_heap src_heap;
-        device->get_descriptor_heap_offset(copy.source_table, copy.source_binding, copy.source_array_offset, &src_heap, &src_offset);
+        descriptor_pool src_heap;
+        device->get_descriptor_pool_offset(copy.source_set, copy.source_binding, copy.source_array_offset, &src_heap, &src_offset);
 
         uint32_t dst_offset;
-        descriptor_heap dst_heap;
-        device->get_descriptor_heap_offset(copy.dest_table, copy.dest_binding, copy.dest_array_offset, &dst_heap, &dst_offset);
+        descriptor_pool dst_heap;
+        device->get_descriptor_pool_offset(copy.dest_set, copy.dest_binding, copy.dest_array_offset, &dst_heap, &dst_offset);
 
-        descriptor_heap_data& src_pool_data = ctx.heaps[src_heap];
-        descriptor_heap_data& dst_pool_data = ctx.heaps[dst_heap];
+        descriptor_pool_data& src_pool_data = ctx.heaps[src_heap];
+        descriptor_pool_data& dst_pool_data = ctx.heaps[dst_heap];
 
         if (dst_offset + copy.count > dst_pool_data.descriptors.size())
             dst_pool_data.descriptors.grow_to_at_least(dst_offset + copy.count);
@@ -132,17 +132,17 @@ bool descriptor_tracking::on_copy_descriptor_tables(device* device, uint32_t cou
     return false;
 }
 
-bool descriptor_tracking::on_update_descriptor_tables(device* device, uint32_t count, const descriptor_table_update* updates) {
+bool descriptor_tracking::on_update_descriptor_sets(device* device, uint32_t count, const descriptor_set_update* updates) {
     descriptor_tracking& ctx = device->get_private_data<descriptor_tracking>();
 
     for (uint32_t i = 0; i < count; ++i) {
-        const descriptor_table_update& update = updates[i];
+        const descriptor_set_update& update = updates[i];
 
         uint32_t offset;
-        descriptor_heap heap;
-        device->get_descriptor_heap_offset(update.table, update.binding, update.array_offset, &heap, &offset);
+        descriptor_pool heap;
+        device->get_descriptor_pool_offset(update.set, update.binding, update.array_offset, &heap, &offset);
 
-        descriptor_heap_data& heap_data = ctx.heaps[heap];
+        descriptor_pool_data& heap_data = ctx.heaps[heap];
 
         if (offset + update.count > heap_data.descriptors.size())
             heap_data.descriptors.grow_to_at_least(offset + update.count);
@@ -182,8 +182,8 @@ void descriptor_tracking::register_events(bool track_descriptors) {
     reshade::register_event<reshade::addon_event::destroy_pipeline_layout>(on_destroy_pipeline_layout);
 
     if (track_descriptors) {
-        reshade::register_event<reshade::addon_event::copy_descriptor_tables>(on_copy_descriptor_tables);
-        reshade::register_event<reshade::addon_event::update_descriptor_tables>(on_update_descriptor_tables);
+        reshade::register_event<reshade::addon_event::copy_descriptor_sets>(on_copy_descriptor_sets);
+        reshade::register_event<reshade::addon_event::update_descriptor_sets>(on_update_descriptor_sets);
     }
 }
 void descriptor_tracking::unregister_events(bool track_descriptors) {
@@ -193,7 +193,7 @@ void descriptor_tracking::unregister_events(bool track_descriptors) {
     reshade::unregister_event<reshade::addon_event::destroy_pipeline_layout>(on_destroy_pipeline_layout);
 
     if (track_descriptors) {
-        reshade::unregister_event<reshade::addon_event::copy_descriptor_tables>(on_copy_descriptor_tables);
-        reshade::unregister_event<reshade::addon_event::update_descriptor_tables>(on_update_descriptor_tables);
+        reshade::unregister_event<reshade::addon_event::copy_descriptor_sets>(on_copy_descriptor_sets);
+        reshade::unregister_event<reshade::addon_event::update_descriptor_sets>(on_update_descriptor_sets);
     }
 }
